a241b61b91852fe20fb0268ea9a83b0c42b0c8b5
BhavyaKanodia <kanodiabhavya@gmail.com>
2021-08-24 22:43:54 +0530

    Section 4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import React from 'react';
+
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
     const expenses = [
@@ -23,9 +26,21 @@ function App() {
         },
     ];
 
+    const addExpenseHandler = (expenses) => {
+        console.log('In App.js');
+        console.log(expenses);
+    };
+
+    // return React.createElement(
+    //     'div',
+    //     {},
+    //     React.createElement('h2', {}, "Let's get started"),
+    //     React.createElement(Expenses, { items: expenses })
+    // );
+
     return (
         <div>
-            <h2>Let's get started!</h2>
+            <NewExpense onAddExpense={addExpenseHandler} />
             <Expenses items={expenses} />
         </div>
     );
