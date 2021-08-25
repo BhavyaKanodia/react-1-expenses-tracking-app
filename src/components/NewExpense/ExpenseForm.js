a7556022a42e6c167c6121476af8f3d1ae966dc4
BhavyaKanodia <kanodiabhavya@gmail.com>
2021-08-25 18:36:43 +0530

    Section 5

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -56,7 +56,7 @@ const ExpenseForm = (props) => {
 
         const expenseData = {
             title: enteredTitle,
-            amount: enteredAmount,
+            amount: +enteredAmount,
             date: new Date(enteredDate),
         };
 
@@ -72,7 +72,11 @@ const ExpenseForm = (props) => {
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
+                    <input
+                        type="text"
+                        value={enteredTitle}
+                        onChange={titleChangeHandler}
+                    />
                     {/* Value for two way binding */}
                 </div>
                 <div className="new-expense__control">
@@ -97,6 +101,9 @@ const ExpenseForm = (props) => {
                 </div>
             </div>
             <div className="new-expense__actions">
+                <button type="button" onClick={props.onCancel}>
+                    Cancel
+                </button>
                 <button type="submit">Add Expense</button>
             </div>
         </form>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false);
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString(),
         };
         props.onAddExpense(expenseData);
+        setIsEditing(false);
+    };
+
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    };
+
+    const stopEditingHandler = () => {
+        setIsEditing(false);
     };
 
     return (
         <div className="new-expense">
+            {!isEditing && (
+                <button onClick={startEditingHandler}>Add New Expense</button>
+            )}
             {/* onSaveExpenseData will be our custom event listener to pass the data from ExpenseForm (child) to NewExpense (parent). This onSaveExpenseData will be passed in as a prop into ExpenseForm */}
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {isEditing && (
+                <ExpenseForm
+                    onCancel={stopEditingHandler}
+                    onSaveExpenseData={saveExpenseDataHandler}
+                />
+            )}
         </div>
     );
 };
